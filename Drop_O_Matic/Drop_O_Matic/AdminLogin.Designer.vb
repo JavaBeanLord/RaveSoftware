13b8a2de1113a1cc044c2f0590a2a69190ec16b2
Justin <justinrgarr@zonecomputers.org>
2014-06-02 20:59:42 -0400

    .

--- Drop_O_Matic/Drop_O_Matic/AdminLogin.Designer.vb
+++ Drop_O_Matic/Drop_O_Matic/AdminLogin.Designer.vb
@@ -53,6 +53,7 @@ Partial Class AdminLogin
         '
         'AdminLogin
         '
+        Me.AcceptButton = Me.Button1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(295, 42)
--- Drop_O_Matic/Drop_O_Matic/Games.vb
+++ Drop_O_Matic/Drop_O_Matic/Games.vb
@@ -6,6 +6,17 @@
     End Sub
 
     Private Sub Games_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MainShow.Hide()
         ToolTip1.SetToolTip(Me.PictureBox1, "The Best Game in The Wild West!")
+
+
+
+
+        ToolTip1.SetToolTip(Me.PictureBox8, "Next Page")
+        ToolTip1.SetToolTip(Me.PictureBox7, "Back Page")
     End Sub
+    Private Sub Games_Close(sender As Object, e As EventArgs) Handles MyBase.FormClosing
+        MainShow.Show()
+    End Sub
+
 End Class
